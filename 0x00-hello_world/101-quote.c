85a742a96a188ba8b4807be60af78b7c77c6a024
juman2312 <jumanhassan47@gmail.com>
2023-08-13 06:19:27 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Description: A C program that prints a line to the standard error
- * Return 1 (not Success)
-*/
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
