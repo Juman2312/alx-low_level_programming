dc6689b436c9b9ca87a47a64471fcadde34fc676
juman2312 <jumanhassan47@gmail.com>
2023-08-18 05:32:22 -0700

    l

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - fjiitm jitm iijm
+ *
+ * @size: is a size oiijjr irj
+ *
+ * Return: always 0 success
+*/
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size; column++)
+			_putchar('#');
+		_putchar('\n');
+
+	}
+
+
+}
