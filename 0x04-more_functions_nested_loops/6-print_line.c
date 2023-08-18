bc57b29eea04616d38d3b2b591012c830f53e0ee
juman2312 <jumanhassan47@gmail.com>
2023-08-17 18:55:31 -0700

    l

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - jhhksgkkhhsllhshsv
+ *
+ * @n: is a number of times huuggffuyy
+ *		jhkhkhkjbbhg
+*/
+
+void print_line(int n);
+{
+	int lnChar;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChar = 1; lnChar <= n; lnChar++)
+			_putchar('_');
+		_putchar('\n');
+	}
+
+}
