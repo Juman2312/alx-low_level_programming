c570f833168db5b8cd6468fa276b839676652aef
juman2312 <jumanhassan47@gmail.com>
2023-08-20 06:11:58 -0700

    l

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - yuhuug hbhjnb hhn
+ *
+ * Return: alwaays return 0 success
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar('\n');
+}
