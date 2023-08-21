24b4dab195585628c8bf78af77a18a463e1b6538
juman2312 <jumanhassan47@gmail.com>
2023-08-21 14:40:37 -0700

    p

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+#include <stdlib>
+#include <time.h>
+
+
+/**
+ * main - bvhng nbhgjm njmk
+ * Return: zero
+*/
+
+
+int main(void)
+{
+	int sum;
+	char c;
+
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
