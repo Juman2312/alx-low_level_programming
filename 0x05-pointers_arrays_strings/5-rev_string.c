b7eed30466cf02146c6a5a9bfe1f13807d7d59d7
juman2312 <jumanhassan47@gmail.com>
2023-08-21 12:20:31 -0700

    l

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,18 @@
 
 void rev_string(char *s)
 {
-	int 1, i;
+	int l, i;
 	char temp;
 
 	/*nvhb bvhgn nhvb bhnj*/
-	for (1 = 0; s[1] != '\0'; ++1)
-
+	for (l = 0; s[l] != '\0'; ++l)
+		;
 
 	/*jgnh nbhgj nghmj jnjg ngjkfj*/
 	for (i = 0; i < 1 / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[1 - 1 - i]; /*-1 nhbg nhgbf nhjmn jm 0*/
-		s[1 - 1 - i] = temp;
+		s[i] = s[l - l - i]; /*-l nhbg nhgbf nhjmn jm 0*/
+		s[l - l - i] = temp;
 	}
 }
