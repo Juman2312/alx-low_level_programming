2d80ebe3bb17dcb3f847dc52f52db71caecac915
juman2312 <jumanhassan47@gmail.com>
2023-08-21 04:55:19 -0700

    l

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - jj bfhfh bfbfbgg nfhfhf nnfh
+ *
+ * @n: hhfgb bvgfgdh bvgffb
+ *
+ * Return: ndhdb nvhffddnh
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar -  jjglkjjtk jghgh hgjgj
+ * @c: hf bhfh hffg
+ *
+ * Return: mhg nbhg nghgh
+ * on error gkgkhnfnf nn
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
