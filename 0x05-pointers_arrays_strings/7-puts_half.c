99972ced3910cc68fe36b688637e5a9f1b2d2437
juman2312 <jumanhassan47@gmail.com>
2023-08-21 13:10:30 -0700

    l

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts-half - nhjm njmh bhn njmk
+ *
+ * @str: bnhj njmkh bhnmj
+ *
+ * Description: gndkgj nhjm njm
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(srt[i]);
+	}
+	_putchar('\n');
+}
