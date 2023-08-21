7d0d88b88c7cb5364b04e38d74ec09c78965c6a3
juman2312 <jumanhassan47@gmail.com>
2023-08-21 05:18:13 -0700

    l

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * swap_int - hgt bfftrg bvgvgft
+ *		ghghh bhgyy hjh
+ *
+ * @a: hgyt bghy bh
+ * @b: bghg bhgt bh nhj
+ *
+ * Return: bdgdt bdgdy bdgdt
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+
