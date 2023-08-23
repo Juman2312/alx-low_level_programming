cb86526537c1acbb6acf9dfd616b596541ca20e7
juman2312 <jumanhassan47@gmail.com>
2023-08-23 12:11:48 -0700

    l

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+
+/**
+ * _putchar - bghn bnhjm njm bgh kl
+ * @c: nhjmn nhj mjh nhj mjkl bgh
+ *
+ * Return: jhnbg bhnj b
+ * lkmjnh nhjm njmh nhjg vgbhn jhg
+*/
+
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
