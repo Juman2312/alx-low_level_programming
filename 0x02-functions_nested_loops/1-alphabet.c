9959957357dbdbabbcba316af78ec7606af7c556
juman2312 <jumanhassan47@gmail.com>
2023-08-30 13:49:02 -0700

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - yuhuug hbhjnb hhn
- *
- * Return: alwaays return 0 success
-*/
+ * print_alphabet - print all alphabet in lowercase
+ */
 
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar('\n');
+		_putchar(letter);
+
+	_putchar('\n');
 }
