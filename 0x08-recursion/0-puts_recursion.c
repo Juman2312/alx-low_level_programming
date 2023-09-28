281e8215aed12061daeabdb68a4a591cf2a96175
Juman2312 <jumanhassan47@gmail.com>
2023-09-28 07:56:39 -0700

    rec

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: a string to be printed
+ * Return: always return 0
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '/0')
+{
+_putchar('/n');
+return (0);
+}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
